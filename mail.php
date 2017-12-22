70b2941e3ae4ebba086093e50de5eb73840e060a
Jaime <jgonzalez1@uc.cl>
2017-12-22 07:41:34 -0300

    changes to email

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "support@wpocean.com";
+        $recipient = "jaimegoweb@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
